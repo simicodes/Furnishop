f448374731367e24617116156e39503177d947d1
simicodes <simeonoffiong@gmail.com>
2022-07-16 10:47:30 -0600

    Features component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 // import components
 import Hero from "./components/Hero";
 import Header from "./components/Header";
+import Features from "./components/Features";
 
 const App = () => {
   return (
     <div className="w-full max-w-[1440px] mx-auto bg-white">
       <Header />
       <Hero />
+      <Features />
     </div>
   );
 };
