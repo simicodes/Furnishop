0184258f2d8b1cd274e9686b68237c347bbf4047
simicodes <simeonoffiong@gmail.com>
2022-07-18 02:56:14 -0600

    Features component

--- src/components/Features.js
+++ src/components/Features.js
@@ -23,13 +23,17 @@ const Features = () => {
               {items.map((item, index) => {
                 //destructure items
                 const { title, subtitle, icon } = item;
-                return <div className="flex mb-6">
-                  <div>{icon}</div>
-                  <div>
-                    <h4>{title}</h4>
-                    <p>{subtitle}</p>
+                return (
+                  <div className="flex mb-6 lg:last:mb-0">
+                    <div className="text-2xl mr-4 lg:text-3xl">{icon}</div>
+                    <div>
+                      <h4 className="text-base lg:text-xl font-semibold mb-3">
+                        {title}
+                      </h4>
+                      <p>{subtitle}</p>
+                    </div>
                   </div>
-                </div>
+                );
               })}
             </div>
           </div>
